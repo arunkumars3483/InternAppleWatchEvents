79099bb071c248b6c37a22af7c640444470e9da6
Arun Kumar <arun@Aruns-Mac-Pro.local>
2015-07-16 15:27:02 +0530

    mappp

---
+++ First/MapScreenControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapScreenControllerViewController.h
+//  First
+//
+//  Created by Arun Kumar on 16/07/15.
+//  Copyright © 2015 Arun Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapScreenControllerViewController : UIViewController
+
+@end
---
+++ First/MapScreenControllerViewController.m
@@ -0,0 +1,37 @@
+//
+//  MapScreenControllerViewController.m
+//  First
+//
+//  Created by Arun Kumar on 16/07/15.
+//  Copyright © 2015 Arun Kumar. All rights reserved.
+//
+
+#import "MapScreenControllerViewController.h"
+
+@interface MapScreenControllerViewController ()
+
+@end
+
+@implementation MapScreenControllerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
