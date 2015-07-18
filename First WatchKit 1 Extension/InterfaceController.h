ad5280e94e62044e8e65fd4e4e19393b317f70d0
Arun Kumar <arun@Aruns-Mac-Pro.local>
2015-07-18 09:51:48 +0530

    watch app

---
+++ First WatchKit 1 Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  First WatchKit 1 Extension
+//
+//  Created by Arun Kumar on 18/07/15.
+//  Copyright © 2015 Arun Kumar. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
