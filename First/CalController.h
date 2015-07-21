8e27023a2bd5aaa9942ceb2b37d7c0c9d6b9af7e
Arun Kumar <arun@Aruns-Mac-Pro.local>
2015-07-21 12:18:43 +0530

    xfgh

---
+++ First/CalController.h
@@ -0,0 +1,19 @@
+//
+//  CalController.h
+//  First
+//
+//  Created by Arun Kumar on 21/07/15.
+//  Copyright © 2015 Arun Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalController : UIViewController
+
+
+-(IBAction)disp;
+
+@property (nonatomic, weak)  UITextField *tt;
+@property (nonatomic, weak) IBOutlet UILabel *ll;
+@property (nonatomic, weak)  UITextField *l2;
+@end
