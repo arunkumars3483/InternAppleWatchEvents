1c8a0c4794d5d20c5c64fb25a181c8b91658a01e
Arun Kumar <arun@Aruns-Mac-Pro.local>
2015-07-21 12:17:59 +0530

    rt

---
+++ First/Event.h
@@ -0,0 +1,25 @@
+//
+//  Event.h
+//  First
+//
+//  Created by Arun Kumar on 21/07/15.
+//  Copyright © 2015 Arun Kumar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Event : NSObject
+{
+@public
+    NSString *address;
+    NSString *date;
+    NSString *desc;
+    NSString *ID;
+    NSString *location;
+    NSString *name;
+    NSString *rating;
+    NSString *time;
+
+
+}
+@end
