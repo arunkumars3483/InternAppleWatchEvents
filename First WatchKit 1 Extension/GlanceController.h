7dd2794aaf102b907fe9c3ab87f06fec320a06f7
Arun Kumar <arun@Aruns-Mac-Pro.local>
2015-07-29 10:32:05 +0530

    Adding files

---
+++ First WatchKit 1 Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  First WatchKit 1 Extension
+//
+//  Created by Arun Kumar on 22/07/15.
+//  Copyright © 2015 Arun Kumar. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
