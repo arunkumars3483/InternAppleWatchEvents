df2a5ba006ff4868fb534b429c48e4d57dfc961f
Arun Kumar <arun@Aruns-Mac-Pro.local>
2015-07-29 13:56:31 +0530

    core data

---
+++ First/Events.m
@@ -0,0 +1,23 @@
+//
+//  Events.m
+//  First
+//
+//  Created by Arun Kumar on 21/07/15.
+//  Copyright © 2015 Arun Kumar. All rights reserved.
+//
+
+#import "Events.h"
+
+@implementation Events
+
+@dynamic desc;
+@dynamic eventId;
+@dynamic location;
+@dynamic time;
+@dynamic address;
+@dynamic name;
+@dynamic date;
+@dynamic rating;
+
+
+@end
